ec29e305113080fa5088b05e2a57c459cf50524d
ammanabrolua <aditya.ammanabrolu@tib.eu>
2022-07-13 19:49:11 +0200

    fix logic

--- dspace-api/src/main/java/org/dspace/administer/CreateAdministrator.java
+++ dspace-api/src/main/java/org/dspace/administer/CreateAdministrator.java
@@ -155,7 +155,7 @@ public final class CreateAdministrator {
                     && cfg.getProperty("webui.supported.locales") == null))) {
             flag = true;
         } else if (!line.hasOption('p') && !line.hasOption("e")  && !line.hasOption("f") && !line.hasOption("l")
-                    && !(line.hasOption("c") || (!line.hasOption("c")
+                    && !(line.hasOption("c") || !(!line.hasOption("c")
                     && cfg.getProperty("webui.supported.locales") == null))) {
             flag = false;
         }
