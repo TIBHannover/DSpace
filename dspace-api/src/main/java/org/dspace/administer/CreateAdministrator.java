94649134c37a1e1a2ea6fc3f1c0d0ad6ba881c2c
ammanabrolua <aditya.ammanabrolu@tib.eu>
2022-08-03 09:38:40 +0200

    chngs

--- dspace-api/src/main/java/org/dspace/administer/CreateAdministrator.java
+++ dspace-api/src/main/java/org/dspace/administer/CreateAdministrator.java
@@ -229,7 +229,7 @@ public final class CreateAdministrator {
                 } 
           
         }
-        if(!flag) {
+        if(!flag && !dataOK) {
             password = getPassword(console);
             if(password == null) {
               return;  
